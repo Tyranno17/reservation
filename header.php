df22c66f208e1f54faa030f96691ffac1ce33938
Tyranno17 <131784210+Tyranno17@users.noreply.github.com>
2023-07-09 20:16:39 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -34,6 +34,9 @@
                             <a class="nav-link" href="calendrier.php">Calendrier</a>
                         </li>
                         <li class="nav-item">
+                            <a class="nav-link" href="reservations.php">Mes réservations</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" href="logout.php">Deconnexion</a>
                         </li>
                     </ul>
